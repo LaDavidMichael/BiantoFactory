b8647b8922f462f53118c52a7f4f998d4184f689
La David Michael <apetmichael@gmail.com>
2021-03-21 19:57:44 +0700

    Title Icon

--- tentangkami.php
+++ tentangkami.php
@@ -19,7 +19,9 @@ if (mysqli_num_rows($query1) > 0) {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Tentang Kami CV.Bianto Factory</title>
+            <title>Tentang Kami - CV.Bianto Factory</title>
+
+            <link rel="icon" href="assets/images/logo.png" type="image/ico" />
             <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
         </head>
