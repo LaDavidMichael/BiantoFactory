b21ec0a5e48394cdd96e89afd984ff57bfb04c6e
La David Michael <apetmichael@gmail.com>
2021-03-22 14:34:07 +0700

    Update Code

--- artikel.php
+++ artikel.php
@@ -14,6 +14,69 @@ error_reporting(0);
     <title>Artikel - CV. Bianto Factory</title>
     <link rel="icon" href="assets/images/logo.png" type="image/ico" />
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <script>
+        function AddReadMore() {
+            //This limit you can set after how much characters you want to show Read More.
+            var carLmt = 280;
+            // Text to show when text is collapsed
+            var readMoreTxt = " ... Read More";
+            // Text to show when text is expanded
+            var readLessTxt = " Read Less";
+
+
+            //Traverse all selectors with this class and manupulate HTML part to show Read More
+            $(".addReadMore").each(function() {
+                if ($(this).find(".firstSec").length)
+                    return;
+
+                var allstr = $(this).text();
+                if (allstr.length > carLmt) {
+                    var firstSet = allstr.substring(0, carLmt);
+                    var secdHalf = allstr.substring(carLmt, allstr.length);
+                    var strtoadd = firstSet + "<span class='SecSec'>" + secdHalf + "</span><span class='readMore'  title='Click to Show More'>" + readMoreTxt + "</span><span class='readLess' title='Click to Show Less'>" + readLessTxt + "</span>";
+                    $(this).html(strtoadd);
+                }
+
+            });
+            //Read More and Read Less Click Event binding
+            $(document).on("click", ".readMore,.readLess", function() {
+                $(this).closest(".addReadMore").toggleClass("showlesscontent showmorecontent");
+            });
+        }
+        $(function() {
+            //Calling function after Page Load
+            AddReadMore();
+        });
+    </script>
+
+    <style>
+        .addReadMore.showlesscontent .SecSec,
+        .addReadMore.showlesscontent .readLess {
+            display: none;
+        }
+
+        .addReadMore.showmorecontent .readMore {
+            display: none;
+        }
+
+        .addReadMore .readMore,
+        .addReadMore .readLess {
+            font-weight: bold;
+            margin-left: 2px;
+            color: blue;
+            cursor: pointer;
+        }
+
+        .addReadMoreWrapTxt.showmorecontent .SecSec,
+        .addReadMoreWrapTxt.showmorecontent .readLess {
+            display: block;
+        }
+    </style>
+
+
 </head>
 
 <body>
@@ -23,47 +86,42 @@ error_reporting(0);
     <section class="text-gray-600 body-font overflow-hidden">
         <div class="container px-5 py-24 mx-auto">
             <div class="-my-8 divide-y-2 divide-gray-100">
-            <div class="container px-5 py-1 mx-auto">
-            <div class="flex flex-col text-center w-full mb-5">
-                <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">ARTIKEL</h1>
-            </div>
-            <section class="text-gray-600 body-font overflow-hidden">
-                <div class="container px-5 py-10 mx-auto">
-                    <div class="-my-8 divide-y-2 divide-gray-100">
-                    <?php
-                    $sql = mysqli_query($koneksidb, "SELECT * FROM artikel ORDER BY id DESC") or die(mysqli_error($koneksidb));
-                    
-                    while ($data = mysqli_fetch_assoc($sql)) {
-                    ?>
-                    <div class="py-8 flex flex-wrap md:flex-nowrap">
-                        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                        <img class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"src="admin/assets/images/artikel/<?= $data['gambar'] ?>" alt="content">
-                        <span class="mt-1 text-gray-500 text-sm "><?= "{$data['tanggal']}" ?></span>
-                            </div>
-                            <div class="md:flex-grow">
-                            <h2 class="text-2xl font-medium text-gray-900 title-font mb-2"><?= $data['judul'] ?></h2>
-                            <p class="leading-relaxed"><?= $data['deskripsi'] ?></p>
-                            <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
-                                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M5 12h14"></path>
-                                <path d="M12 5l7 7-7 7"></path>
-                                </svg>
-                            </a>
-                            </div>
+                <div class="container px-5 py-1 mx-auto">
+                    <div class="flex flex-col text-center w-full mb-5">
+                        <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">ARTIKEL</h1>
                     </div>
-            
-                    <?php
-                    }
-                ?>
-                    </div>
+                    <section class="text-gray-600 body-font overflow-hidden">
+                        <div class="container px-5 py-10 mx-auto">
+                            <div class="-my-8 divide-y-2 divide-gray-100">
+                                <?php
+                                $sql = mysqli_query($koneksidb, "SELECT * FROM artikel ORDER BY id DESC") or die(mysqli_error($koneksidb));
+
+                                while ($data = mysqli_fetch_assoc($sql)) {
+                                ?>
+                                    <div class="py-8 flex flex-wrap md:flex-nowrap">
+                                        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                                            <img class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="admin/assets/images/artikel/<?= $data['gambar'] ?>" alt="content">
+                                            <span class="mt-1 text-gray-500 text-sm "><?= "{$data['tanggal']}" ?></span>
+                                        </div>
+                                        <div class="md:flex-grow">
+                                            <h2 class="text-2xl font-medium text-gray-900 title-font mb-2"><?= $data['judul'] ?></h2>
+                                            <p class="addReadMore showlesscontent"> <?= $data['deskripsi'] ?> </p>
+                                        </div>
+                                    </div>
+
+                                <?php
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </section>
                 </div>
-            </section>
             </div>
-            </div>
-         </div>
-        </body>
-        
-    <!-- Footer -->
-    <?php include('includes/footer.php'); ?>
-    <!-- /Footer -->
+        </div>
+</body>
+
+<!-- Footer -->
+<?php include('includes/footer.php'); ?>
+<!-- /Footer -->
+
 </html>
