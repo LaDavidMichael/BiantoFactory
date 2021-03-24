a670cd42cdb624464fab47b7390d1b073eb8ca32
ynos <68013874+sonyrezeky8@users.noreply.github.com>
2021-03-25 01:02:50 +0700

    database sosmed

--- database/biantofactory.sql
+++ database/biantofactory.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Mar 2021 pada 17.04
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 8.0.0
+-- Generation Time: Mar 24, 2021 at 07:00 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -35,7 +36,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `updateOnDate`) VALUES
@@ -44,7 +45,7 @@ INSERT INTO `admin` (`id`, `username`, `password`, `updateOnDate`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `artikel`
+-- Table structure for table `artikel`
 --
 
 CREATE TABLE `artikel` (
@@ -56,19 +57,19 @@ CREATE TABLE `artikel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `artikel`
+-- Dumping data for table `artikel`
 --
 
 INSERT INTO `artikel` (`id`, `tanggal`, `judul`, `deskripsi`, `gambar`) VALUES
-(4, '2021-03-02', 'Water Heater', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 0x622e706e67),
-(47, '2021-03-22', 'NoxxleHeater', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 0x4e6f78786c654865617465722e6a7067),
-(48, '2021-03-22', 'BandHeater', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 0x42616e644865617465722e6a7067),
-(49, '2021-03-22', 'ImmersionHeater', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 0x496d6d657273696f6e4865617465722e6a7067);
+(4, '2021-03-22', 'Tubular Heater', 'Elemen pemanas berbentuk tabung yang dibangun dari selubung tabung logam dari bahan yang dipilih untuk memenuhi aplikasi tertentu. Selubung ini, mengelilingi kumparan heliks dari nikel-krom kawat resistensi, berpusat dan ketat dipadatkan dengan insulator listrik dan media transfer panas dari magnesium oksida (MgO). Hal ini memberikan spiral resistensi yang dapat bervariasi dalam konsentrasi panas dan bagian panjang . hampir semua bentuk atau ukuran Diameter  kustom dapat dibuat atas permintaan. Biasanya menggunakan baja, stainless steel, Incoloy, Inconel atau paduan titanium, elemen tubular sering dianggap sebagai dasar dari semua elemen pemanas. Elemen-elemen pemanas memiliki selubung luar yang kuat untuk membantu melindungi pemanas dari stres fisik dan menggunakan paduan kualitas tinggi untuk memungkinkan transfer panas yang efisien dari koil resistansi terhadap media pemanas Anda.', 0x547562756c61724865617465722e6a7067),
+(47, '2021-03-22', 'Nozzle Heater', 'Nozzle heater dipergunakan untuk memanaskan tabung, perbedaan nya diameter nozzle heater lebih kecil dari 50 mm.  Nozzle heater paling banyak dipergunakan untuk barrel mesin extruder dan injection plastik, pada barrel nya dipasang band heater dan pada ujung keluaran cairan platik (nozzle) di pasang nozzle heater.', 0x4e6f7a7a6c654865617465722e6a7067),
+(48, '2021-03-22', 'Band Heater', 'Band Heater adalah pemanas yang biasanya digunakan untuk aplikasi pengolahan plastik dalam bentuk silinder, pada umumnya dimensi yang sering digunakan pada industri mulai dari Ø 25 sampai dengan 500 mm. Band Heater tentunya memiliki keterbatasan apabila dibandingkan dengan band heater keramik. Daya yang dihasilkan mulai dari 50 – 10.000 Watt dapat diaplikasikan pada mesin-mesin injeksi/ekstruder.', 0x42616e644865617465722e6a7067),
+(49, '2021-03-22', 'Immersion Heater', '  Penggunaan pemanas celup telah lebih populer dalam beberapa dekade terakhir karena meningkatnya biaya bahan bakar memaksa individu untuk memilih cara yang lebih efisien untuk biaya pemanasan aplikasi mereka. Secara ekologis kelompok consciencious telah lama menganjurkan untuk menggunakan energi yang bersih untuk membantu menyelamatkan lingkungan. Pemanas listrik telah dikenal sebagai bentuk terbersih pemanasan tidak meninggalkan residu dan  menyediakan transfer panas langsung ke media apapun.  ', 0x496d6d657273696f6e4865617465722e6a7067);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `contact`
+-- Table structure for table `contact`
 --
 
 CREATE TABLE `contact` (
@@ -79,16 +80,16 @@ CREATE TABLE `contact` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `contact`
+-- Dumping data for table `contact`
 --
 
 INSERT INTO `contact` (`id`, `alamat`, `tlpn`, `email`) VALUES
-(1, '  Jalan Kyai Satari II/14B, Kel. Rungkut Menanggal, Kec. Gunung Anyar, Kota Surabaya', '081357047260', 'biantofactory@gmail.com');
+(1, 'Jalan Kyai Satari II/14B, Kel. Rungkut Menanggal, Kec. Gunung Anyar, Kota Surabaya ', '081357047260', 'biantofactory@gmail.com');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `feedback`
+-- Table structure for table `feedback`
 --
 
 CREATE TABLE `feedback` (
@@ -96,14 +97,13 @@ CREATE TABLE `feedback` (
   `nama` varchar(255) NOT NULL,
   `hp` varchar(13) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `pesan` longtext NOT NULL,
-  `status` int(11) DEFAULT NULL
+  `pesan` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `misi`
+-- Table structure for table `misi`
 --
 
 CREATE TABLE `misi` (
@@ -112,7 +112,7 @@ CREATE TABLE `misi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `misi`
+-- Dumping data for table `misi`
 --
 
 INSERT INTO `misi` (`id`, `misi`) VALUES
@@ -123,51 +123,46 @@ INSERT INTO `misi` (`id`, `misi`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `profile`
+-- Table structure for table `profile`
 --
 
 CREATE TABLE `profile` (
   `id` int(11) NOT NULL,
   `gambar` blob NOT NULL,
-  `deskripsi` longtext NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `jabatan` varchar(255) NOT NULL,
-  `moto` varchar(255) NOT NULL
+  `deskripsi` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `profile`
+-- Dumping data for table `profile`
 --
 
-INSERT INTO `profile` (`id`, `gambar`, `deskripsi`, `nama`, `jabatan`, `moto`) VALUES
-(1, 0x6c6f676f20362e706e67, '     CV. Bianto Factory merupakan pelopor Produsen Element Pemanas dan Thermocouple di salah satu Kota di Jawa Timur. Dengan lingkup pemasaran seluruh Indonesia. Perusahaan kami berdiri sejak tahun 1995, dengan alamat awal di Jalan Kyai Satari II/41 Surabaya. Pada tahun 2013 kami berpindah tempat, dikarenakan Sertifikat lahan yang masih belum kejelasannya.\r\nDalam perjalannya, CV. Bianto Factory telah mengalami sejumlah perubahan nama, dengan nama awal adalah CV. Tunggal Jaya Elektrik, dan pada tahun 2013 merubah namanya menjadi CV. Bianto Factory.     ', 'Robianto', 'CEO', 'Selalu ingin memberikan yang terbaik untuk bangsa dan negara');
+INSERT INTO `profile` (`id`, `gambar`, `deskripsi`) VALUES
+(1, 0x6c6f676f20362e706e67, ' CV. Bianto Factory merupakan pelopor Produsen Element Pemanas dan Thermocouple di salah satu Kota di Jawa Timur. Dengan lingkup pemasaran seluruh Indonesia. Perusahaan kami berdiri sejak tahun 1995, dengan alamat awal di Jalan Kyai Satari II/41 Surabaya. Pada tahun 2013 kami berpindah tempat, dikarenakan Sertifikat lahan yang masih belum kejelasannya.\r\nDalam perjalannya, CV. Bianto Factory telah mengalami sejumlah perubahan nama, dengan nama awal adalah CV. Tunggal Jaya Elektrik, dan pada tahun 2013 merubah namanya menjadi CV. Bianto Factory. ');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `sosialmedia`
+-- Table structure for table `sosialmedia`
 --
 
 CREATE TABLE `sosialmedia` (
   `id` int(11) NOT NULL,
   `instagram` varchar(255) NOT NULL,
   `facebook` varchar(255) NOT NULL,
-  `twitter` varchar(255) NOT NULL,
-  `linkedin` varchar(255) NOT NULL,
   `youtube` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `sosialmedia`
+-- Dumping data for table `sosialmedia`
 --
 
-INSERT INTO `sosialmedia` (`id`, `instagram`, `facebook`, `twitter`, `linkedin`, `youtube`) VALUES
-(1, 'https://www.instagram.com/', 'https://id-id.facebook.com/', 'https://twitter.com/', 'https://id.linkedin.com/', 'https://www.youtube.com/');
+INSERT INTO `sosialmedia` (`id`, `instagram`, `facebook`, `youtube`) VALUES
+(1, 'https://www.instagram.com/biantofactory/', 'https://web.facebook.com/biantofactoryreal', 'https://www.youtube.com/channel/UCjyQars8F1AA-1LV8w8O2kw');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `team`
+-- Table structure for table `team`
 --
 
 CREATE TABLE `team` (
@@ -181,19 +176,19 @@ CREATE TABLE `team` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `team`
+-- Dumping data for table `team`
 --
 
 INSERT INTO `team` (`id`, `nama`, `jabatan`, `deskripsi`, `foto`, `instagram`, `facebook`) VALUES
-(8, 'Jonas', 'CEO', 'It\'s supposed to be automatic, but actually you have to push this button.', 0x69636f6e642e706e67, '', ''),
-(9, 'Maxine', 'CTO', 'It\'s supposed to be automatic, but actually you have to push this button.', 0x69636f6e632e706e67, '', ''),
-(10, 'Catherine', 'CFO', 'It\'s supposed to be automatic, but actually you have to push this button.', 0x69636f6e622e706e67, '', ''),
-(15, 'Demetrius', 'COO', 'It\'s supposed to be automatic, but actually you have to push this button.', 0x69636f6e612e706e67, '', '');
+(8, 'Teguh', 'Manager Operasional', '\" Kamu jauh lebih kuat dari ketakutanmu \"', 0x63726577342e706e67, '', ''),
+(9, 'Suyanto', 'Planning & Production Operator', '\" Nilaimu tidak berkurang berdasarkan ketidakmampuan seseorang untuk melihat nilaimu \"', 0x63726577312e706e67, '', ''),
+(10, 'Suwito', 'General Mechanic', '\" Kedewasaan bukan dilihat dari usia, melainkan dengan tindakan dan tingkah laku \"', 0x63726577322e706e67, '', ''),
+(15, 'Hariyo', 'Production Operator', '\" Sulit bukan berarti tidak mungkin \"', 0x63726577332e706e67, '', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `testi`
+-- Table structure for table `testi`
 --
 
 CREATE TABLE `testi` (
@@ -206,19 +201,19 @@ CREATE TABLE `testi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `testi`
+-- Dumping data for table `testi`
 --
 
 INSERT INTO `testi` (`id`, `fotosampul`, `kota`, `provinsi`, `judul`, `deskripsi`) VALUES
-(9, 'orangc.png', 'Jakarta', 'DKI Jakarta', 'Eclair Inc', 'Good Product'),
-(11, 'orangb.png', 'Sidoarjo', 'Jawa Timur', 'Cupcake LLC', 'Good Product'),
-(13, 'orang.png', 'Surabaya', 'Jawa Timur', 'Cupcake LLC', 'Good Product'),
-(14, 'oranga.png', 'Bandung', 'Jawa Bandung', 'Cupcake LLC', 'Good Product');
+(9, 'orangc.png', 'Jakarta', 'DKI Jakarta', 'Mitsubishi Motors Corporation', 'Barang bagus dan juga terpercaya'),
+(11, 'orangb.png', 'Sidoarjo', 'Jawa Timur', 'PT. Avia Avian', 'Thanks CV. Avia Avian,\r\nBagus dan Awet'),
+(13, 'orang.png', 'Surabaya', 'Jawa Timur', 'PT. AM Desain Corp', 'Terpercaya, mesinnya bagus, Sukses terus CV Bianto Factory, next pasti order lagi'),
+(14, 'oranga.png', 'Bandung', 'Jawa Bandung', 'PT Astra Honda Motor (AHM)', 'Puas dengan pemanasnya, terima kasih untuk CV. Bianto Factory');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `visi`
+-- Table structure for table `visi`
 --
 
 CREATE TABLE `visi` (
@@ -227,7 +222,7 @@ CREATE TABLE `visi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `visi`
+-- Dumping data for table `visi`
 --
 
 INSERT INTO `visi` (`id`, `visi`) VALUES
@@ -239,116 +234,116 @@ INSERT INTO `visi` (`id`, `visi`) VALUES
 --
 
 --
--- Indeks untuk tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `artikel`
+-- Indexes for table `artikel`
 --
 ALTER TABLE `artikel`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `contact`
+-- Indexes for table `contact`
 --
 ALTER TABLE `contact`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `feedback`
+-- Indexes for table `feedback`
 --
 ALTER TABLE `feedback`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `misi`
+-- Indexes for table `misi`
 --
 ALTER TABLE `misi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `profile`
+-- Indexes for table `profile`
 --
 ALTER TABLE `profile`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `sosialmedia`
+-- Indexes for table `sosialmedia`
 --
 ALTER TABLE `sosialmedia`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `team`
+-- Indexes for table `team`
 --
 ALTER TABLE `team`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `testi`
+-- Indexes for table `testi`
 --
 ALTER TABLE `testi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `visi`
+-- Indexes for table `visi`
 --
 ALTER TABLE `visi`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `admin`
+-- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `artikel`
+-- AUTO_INCREMENT for table `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
--- AUTO_INCREMENT untuk tabel `contact`
+-- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `misi`
+-- AUTO_INCREMENT for table `misi`
 --
 ALTER TABLE `misi`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `profile`
+-- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `team`
+-- AUTO_INCREMENT for table `team`
 --
 ALTER TABLE `team`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT untuk tabel `testi`
+-- AUTO_INCREMENT for table `testi`
 --
 ALTER TABLE `testi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT untuk tabel `visi`
+-- AUTO_INCREMENT for table `visi`
 --
 ALTER TABLE `visi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
