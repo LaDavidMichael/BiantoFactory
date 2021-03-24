d5cd56022ea17d001c55e307bf54c3b6e5e85817
La David Michael <apetmichael@gmail.com>
2021-03-24 20:02:15 +0700

    Update ReadMore adn ReadLess

--- content.php
+++ content.php
@@ -1,96 +1,96 @@
 <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 <br><br>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+<script>
+  function AddReadMore() {
+    //This limit you can set after how much characters you want to show Read More.
+    var carLmt = 110;
+    // Text to show when text is collapsed
+    var readMoreTxt = " ... Baca Selengkapnya";
+    // Text to show when text is expanded
+    var readLessTxt = " Baca Lebih Singkat";
+
+
+    //Traverse all selectors with this class and manupulate HTML part to show Read More
+    $(".addReadMore").each(function() {
+      if ($(this).find(".firstSec").length)
+        return;
+
+      var allstr = $(this).text();
+      if (allstr.length > carLmt) {
+        var firstSet = allstr.substring(0, carLmt);
+        var secdHalf = allstr.substring(carLmt, allstr.length);
+        var strtoadd = firstSet + "<span class='SecSec'>" + secdHalf + "</span><span class='readMore'  title='Click to Show More'>" + readMoreTxt + "</span><span class='readLess' title='Click to Show Less'>" + readLessTxt + "</span>";
+        $(this).html(strtoadd);
+      }
+
+    });
+    //Read More and Read Less Click Event binding
+    $(document).on("click", ".readMore,.readLess", function() {
+      $(this).closest(".addReadMore").toggleClass("showlesscontent showmorecontent");
+    });
+  }
+  $(function() {
+    //Calling function after Page Load
+    AddReadMore();
+  });
+</script>
+
+<style>
+  .addReadMore.showlesscontent .SecSec,
+  .addReadMore.showlesscontent .readLess {
+    display: none;
+  }
+
+  .addReadMore.showmorecontent .readMore {
+    display: none;
+  }
+
+  .addReadMore .readMore,
+  .addReadMore .readLess {
+    font-family: century gothic;
+    font-weight: bold;
+    margin-left: 2px;
+    color: teal;
+    cursor: pointer;
+  }
+
+  .addReadMoreWrapTxt.showmorecontent .SecSec,
+  .addReadMoreWrapTxt.showmorecontent .readLess {
+    display: block;
+  }
+</style>
+
+
 <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-wrap w-full mb-20">
-            <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Ulasan Pelanggan Kami</h1>
-                <div class="h-1 w-20 bg-teal-500 rounded"></div>
-            </div>
-            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Kami selalu ingin memberikan pelayanan yang memuaskan untuk para pelanggan kami, komitmen kami Menjaga kualitas dan kepercayaan konsumen, agar mampu membuat alat pemanas yang sesuai dengan yang dibutuhkan oleh konsumen.</p>
-        </div>
-        <div class="flex flex-wrap -m-4">
-
-            <?php
-            $sql = mysqli_query($koneksidb, "SELECT * FROM testi ORDER BY id DESC") or die(mysqli_error($koneksidb));
-
-            while ($data = mysqli_fetch_assoc($sql)) {
-            ?>
-
-                <div class="xl:w-1/4 md:w-1/2 p-4">
-                    <div class="bg-gray-100 p-6 rounded-lg">
-                        <img class="h-40 rounded w-full object-cover object-center mb-6" src="admin/assets/images/<?= $data['fotosampul'] ?>" alt="content">
-                        <h3 class="tracking-widest text-teal-500 text-xs font-medium title-font"><?= "{$data['kota']}, {$data['provinsi']}" ?></h3>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4"><?= $data['judul'] ?></h2>
-                        <p class="leading-relaxed text-base">"<?= $data['deskripsi'] ?>"</p>
-                    </div>
-                </div>
-            <?php
-            }
-            ?>
-
-        </div>
-        <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-  <div class="flex-1 flex justify-between sm:hidden">
-    <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-      Previous
-    </a>
-    <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-      Next
-    </a>
-  </div>
-  <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-    <div>
-      <p class="text-sm text-gray-700">
-        Showing
-        <span class="font-medium">1</span>
-        to
-        <span class="font-medium">10</span>
-        of
-        <span class="font-medium">97</span>
-        results
-      </p>
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20">
+      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Ulasan Pelanggan Kami</h1>
+        <div class="h-1 w-20 bg-teal-500 rounded"></div>
+      </div>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Kami selalu ingin memberikan pelayanan yang memuaskan untuk para pelanggan kami, komitmen kami Menjaga kualitas dan kepercayaan konsumen, agar mampu membuat alat pemanas yang sesuai dengan yang dibutuhkan oleh konsumen.</p>
     </div>
-    <div>
-      <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-        <a href="#" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-          <span class="sr-only">Previous</span>
-          <!-- Heroicon name: solid/chevron-left -->
-          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-          </svg>
-        </a>
-        <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-          1
-        </a>
-        <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-          2
-        </a>
-        <a href="#" class="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-          3
-        </a>
-        <span class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
-          ...
-        </span>
-        <a href="#" class="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-          8
-        </a>
-        <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-          9
-        </a>
-        <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-          10
-        </a>
-        <a href="#" class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-          <span class="sr-only">Next</span>
-          <!-- Heroicon name: solid/chevron-right -->
-          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-          </svg>
-        </a>
-      </nav>
+
+    <div class="flex flex-wrap -m-4">
+      <?php
+      $sql = mysqli_query($koneksidb, "SELECT * FROM testi ORDER BY id DESC") or die(mysqli_error($koneksidb));
+
+      while ($data = mysqli_fetch_assoc($sql)) {
+      ?>
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <img class="h-40 rounded w-full object-cover object-center mb-6" src="admin/assets/images/<?= $data['fotosampul'] ?>" alt="content">
+            <h3 class="tracking-widest text-teal-500 text-xs font-medium title-font"><?= "{$data['kota']}, {$data['provinsi']}" ?></h3>
+            <h2 class="text-lg text-gray-900 font-medium title-font mb-4"><?= $data['judul'] ?></h2>
+            <p class="addReadMore showlesscontent">"<?= $data['deskripsi'] ?>"</p>
+          </div>
+        </div>
+      <?php
+      }
+      ?>
     </div>
   </div>
-</div>
-    </div>
 </section>
